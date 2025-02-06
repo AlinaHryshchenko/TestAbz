62b536a7a877ce59f461aa5a3e6e91d5b0e18001
Alina Hryshchenko <a.hryshchenko@4.partners>
2025-02-06 22:47:26 +0000

    Первый коммит

---
+++ TestAbz/TestAbzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAbzApp.swift
+//  TestAbz
+//
+//  Created by Alina Hryshchenko on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAbzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
